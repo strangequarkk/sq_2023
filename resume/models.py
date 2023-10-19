c807b8a6dd4e579147f6084e34bde2e96567f846
Kae Unterseher <turningtern@gmail.com>
2023-10-18 19:45:17 -0700

    created model for resume item

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class ResumeItem(models.Model):
+    employer = models.CharField(max_length=255)
+    title = models.CharFiled(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.employer
