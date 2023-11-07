945f65d3bef5a4177607a77cfda6e380191a47a0
Kae Unterseher <turningtern@gmail.com>
2023-11-06 18:03:40 -0800

    Beginning to add CRUD component routes

--- sq-front/src/App.jsx
+++ sq-front/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom'
+import { AddResumeItem } from './components/AddResumeItem'
+import { ResumeList } from './components/ResumeList'
+import { UpdateResume } from './components/UpdateResume'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <nav className="navbar">
+        <a href='/' className="navbar-brand">
+          Resume
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Link exact to={'/add'} className="nav-link">
+          Add
+        </Link>
+      </nav>
+      <Routes>
+        <Route exact path={["/", "/resume"]} element={< ResumeList />} />
+        <Route exact path={["add"]} element={< AddResumeItem />} />
+        <Route path="/resume/:id/update/" component={< UpdateResume/>} />
+      </Routes>
     </>
   )
 }
