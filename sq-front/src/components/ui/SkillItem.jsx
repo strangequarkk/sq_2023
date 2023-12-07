fd188a4c648ca2ff84836e02fd69e42500df27c4
Kae Unterseher <turningtern@gmail.com>
2023-12-07 00:15:57 -0800

    added animated tooltip to selected languages

--- sq-front/src/components/ui/SkillItem.jsx
+++ sq-front/src/components/ui/SkillItem.jsx
@@ -1,14 +1,36 @@
 import PropTypes from 'prop-types'
+import { useState} from 'react'
 
-const SubItem = ({skill, builtWith}) => {
-    const commonClasses ="border border-solid rounded-full py-1 px-2 text-center inline"
-    const defaultItem = <li className="grow " >
+
+const SubItem = ({ skill, builtWith }) => {
+    const [infoClass, setInfoClass] = useState("hidden")
+    const [toolTipWidth, setToolTipWidth] = useState(" w-0 ")
+    const sparkleClasses = " after:content-['✧'] after:text-purple-400 before:content-['✧'] before:text-purple-400 block "
+    const toolTipClasses = " absolute -top-2/3 whitespace-nowrap rounded-full overflow-hidden left-2/3 text-sm transition-all duration-200 p-1 pr-1.5 " + sparkleClasses 
+    
+    const commonClasses = " border border-solid rounded-full py-1 px-2 text-center inline-block "
+
+
+    const handleClick = () => {
+        const toggledClass = infoClass === "invisible" ?
+            " visible border border-solid border-purple-400 shadow-xl bg-white z-10 " 
+            : "invisible";
+        const toggledWidth = infoClass === "invisible" ? " w-60  " : " w-0 " ;
+        setToolTipWidth(toggledWidth)
+        setInfoClass(toggledClass)
+    }
+
+ 
+    
+    
+    const defaultItem = <li className=" grow " >
         <p className={commonClasses + " border-teal-200"}>{skill}</p>
     </li>;
-    const specialItem = <li className="grow ">
-        <p className={commonClasses +  " border-purple-400"}>{skill}</p>
-        <span className="hidden">This site was built with {skill}</span>
-    </li>
+    const specialItem = <li className="grow">
+        <p className={commonClasses + " border-purple-400 relative cursor-pointer"} onClick={handleClick}>{skill}
+            <span className={ infoClass + toolTipClasses + toolTipWidth}>This site was built with {skill}!</span>
+            </p>
+    </li>;
     const displayedItem = builtWith ? specialItem : defaultItem;
     return (
         <>{displayedItem}</>
