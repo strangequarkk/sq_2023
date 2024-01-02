af41b621477f2f3b712e1c2764ca2897b28154db
Kae Unterseher <turningtern@gmail.com>
2024-01-01 22:30:55 -0800

    finished styling review card carousel

--- sq-front/src/components/sections/Experience/ResumeItem.jsx
+++ sq-front/src/components/sections/Experience/ResumeItem.jsx
@@ -3,13 +3,12 @@ export const ResumeItem = ({ title, employer, description, start_date, end_date}
     const start = start_date.split('-')[0]
     const end = end_date.split('-')[0]
     return (
-        <article className="resume-list-item bg-white/50 rounded p-2 mt-3">
+        <article className="resume-list-item p-2 mt-3">
               <h2 className="italic text-lg" >{employer}</h2>
             
-            <p className="flex justify-between text-sm italic px-2 m-auto"><span>{start}</span>
-                {end}</p>
+            <p className="text-right text-sm italic w-100 m-auto"><span>{start}</span> - <span>{end}</span></p>
                 <h4 className="font-bold">{title}</h4>
-              <p className="text-sm p-1">{description}</p>
+              <p className="text-sm  bg-white/50 rounded-lg p-2">{description}</p>
 
             </article>
     )
--- sq-front/src/components/sections/Reviews/Reviews.jsx
+++ sq-front/src/components/sections/Reviews/Reviews.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState, } from "react";
 import { retrieveAllReviews } from "../../../services/review.service"
 import { ReviewCard } from "./ReviewCard";
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import LeftArrow from '../../../assets/arrow-chevron-left.svg'
+import RightArrow from '../../../assets/arrow-chevron-right.svg'
+import './reviews-style.css';
+
 export const Reviews = () => {
     const [reviews, setReviews] = useState({})
     
@@ -16,7 +21,7 @@ export const Reviews = () => {
 
     const reviewCards = reviews.length > 0 ? reviews.map((review) => {
         return (<Slide key={review.title}
-            className="border-2 border-solid border-black mx-2 flex"
+            className="border-2 border-solid border-neutral-700 mx-2 flex"
         innerClassName="flex">
             <ReviewCard  {...review} />
         </Slide>)
@@ -27,18 +32,28 @@ export const Reviews = () => {
             <h2 className="font-heading text-3xl mb-3">Reviews</h2>
             <CarouselProvider
         naturalSlideWidth={1}
-        naturalSlideHeight={0}
+        naturalSlideHeight={1}
         totalSlides={reviews.length}
             >
-                <DotGroup dotNumbers={true} className="flex justify-around w-3/4 m-auto" />
-                <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
+                <DotGroup dotNumbers={true} className="flex justify-around align-middle w-3/4 mx-auto text-sm my-2" />
+                <div className="relative">
+                    <ButtonBack className="absolute -left-8 top-0 bottom-0 z-40">
+                    <span className=" before:content-[' '] before:bg-white/75 before:rounded-full before:p-24 before:absolute before:top-[20%] before:right-[10%] before:z-0">
+                            <img className="h-14 z-10 relative text-neutral-700" src={LeftArrow} alt="Back" />
+                            </span>
+                    </ButtonBack>
+                    <ButtonNext className="absolute -right-8 top-0 bottom-0 z-40">
+                        <span className=" before:content-[' '] before:bg-white/75 before:rounded-full before:p-24 before:absolute before:top-[20%] before:left-[10%] before:z-0">
+                            <img className="h-14 z-10 relative" src={RightArrow} alt="Next" />
+                        </span>
+                    </ButtonNext>
                 <Slider classNameTray="flex">
                 {reviewCards}
-                </Slider>
+                    </Slider>
+                    </div>
                 
       </CarouselProvider>
-            
+            <a href="wyzant.com/Tutors/KaeTutorsCS" className="text-sm text-center block w-3/4 mx-auto underline pt-3">Check out my Wyzant profile for more student reviews!</a>
         </section>
     )
 }
