269cd2c67a330b863ceceb2fe4df1b0f226dff61
Kae Unterseher <turningtern@gmail.com>
2024-03-30 18:37:34 -0700

    restricting webhook endpoint to github ips

--- strange_quark/views.py
+++ strange_quark/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
+
+import requests
+from ipaddress import ip_address, ip_network
 
 
 @csrf_exempt
 def gitWebHook(request):
+    forwarded_for = "{}".format(request.META.get("HTTP_X_FORWARDED_FOR"))
+    client_ip_address = ip_address(forwarded_for)
+    whitelist = requests.get("https://api.github.com/meta").json()["hooks"]
+
+    for valid_ip in whitelist:
+        if client_ip_address in ip_network(valid_ip):
+            break
+    else:
+        return HttpResponseForbidden("Permission denied")
     return HttpResponse("pong")
