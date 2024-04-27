92e38fa4a16dc5a8eaa5e831129e2aea2ced4a46
Kae Unterseher <turningtern@gmail.com>
2024-04-27 14:59:16 -0700

    test double-binding clickout events

--- sq-front/src/utils/useDetectClickOutTwo.jsx
+++ sq-front/src/utils/useDetectClickOutTwo.jsx
@@ -9,10 +9,13 @@ export const useDetectClickOut = (
     if (enabled) {
       const onClickOut = (event) => {
         if (event.target != clickableElement) {
+          event.preventDefault();
           handleClickOut(event);
         }
       };
+      window.removeEventListener("touchstart", onClickOut);
       window.removeEventListener("click", onClickOut);
+      window.addEventListener("touchstart", onClickOut);
       window.addEventListener("click", onClickOut);
       const remove = () => window.removeEventListener("click", onClickOut);
       return remove;
