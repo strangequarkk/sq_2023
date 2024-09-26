91f57533dc70f89aae1291414ec0159972bfff8e
Kae Unterseher <turningtern@gmail.com>
2024-09-25 20:50:04 -0700

    added npm setup to wsgi

--- strange_quark/asgi.py
+++ strange_quark/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strange_quark.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "strange_quark.settings")
 
 application = get_asgi_application()
--- strange_quark/wsgi.py
+++ strange_quark/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from common.utils import build_and_collect
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "strange_quark.settings")
 
 application = get_wsgi_application()
+build_and_collect()
