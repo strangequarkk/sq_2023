676944bccb3e84c511a66cd67c9f70ab6bf86646
Kae Unterseher <turningtern@gmail.com>
2024-09-25 22:46:26 -0700

    check node and npm versions in venv/nodeenv

--- common/utils.py
+++ common/utils.py
@@ -5,6 +5,8 @@ import subprocess
 # builds the vite project and then collects static files for django
 def build_and_collect():
     print("build and collect static")
+    subprocess.run(["node", "-v"])
+    subprocess.run(["npm", "-v"])
     # subprocess.run(["nodeenv", "-p", "--with-npm"])
     # subprocess.run(["npm", "install"], cwd=os.getcwd() + "/sq-front")
     # subprocess.run(["npm", "audit", "fix"], cwd=os.getcwd() + "/sq-front")
