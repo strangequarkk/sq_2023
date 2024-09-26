be373504c03d0dad1b9961983c694312e8eb9c8f
Kae Unterseher <turningtern@gmail.com>
2024-09-25 22:45:22 -0700

    if nodeenv is added to the virtual environment in the release tasks, you shouldn't have to reactivate it in build and collect

--- common/utils.py
+++ common/utils.py
@@ -5,9 +5,9 @@ import subprocess
 # builds the vite project and then collects static files for django
 def build_and_collect():
     print("build and collect static")
-    subprocess.run(["nodeenv", "-p", "--with-npm"])
-    subprocess.run(["npm", "install"], cwd=os.getcwd() + "/sq-front")
-    subprocess.run(["npm", "audit", "fix"], cwd=os.getcwd() + "/sq-front")
+    # subprocess.run(["nodeenv", "-p", "--with-npm"])
+    # subprocess.run(["npm", "install"], cwd=os.getcwd() + "/sq-front")
+    # subprocess.run(["npm", "audit", "fix"], cwd=os.getcwd() + "/sq-front")
     subprocess.run(["npm", "run", "build"], cwd=os.getcwd() + "/sq-front")
     subprocess.run(
         ["python3", "manage.py", "collectstatic", "--noinput"], cwd=os.getcwd()
