cdbd6ab12d885242a68af7144877e9fc7d2418fa
Kae Unterseher <turningtern@gmail.com>
2024-10-18 19:29:32 -0700

    giving the heroku approach one more shot

---
+++ release-tasks-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash  
+
+echo "Release scripts:"  
+pip install --no-cache-dir -r requirements.txt
+python manage.py migrate
+nodeenv env --with-npm
+source env/bin/activate
+echo "Release: node/npm installed and active?"
+echo "current directory:"
+echo "$PWD"
+echo `dirname $(realpath $0)`
+cp /etc/.env /strange_quark/.env
+cd /strange_quark
+echo "is env file present???"
+ls
+cd ../
+cd sq-front
+npm install
+npm audit fix
+npm run build
+cd ../
+python manage.py collectstatic --noinput 
+echo "Release scripts complete!"  
+  
+
+
+  
+
